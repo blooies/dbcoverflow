62c2c5dbc08647f291db83b0b0d54312529794a0
Christine Feaster <feasterc@gmail.com>
2014-06-09 09:49:43 -0400

    Redirect route when querying a question that does not exist
    Fix issue where query url for non-existent question directs user to error page.
    When user queries a non-existent number, user is redirected to list of questions.

--- dbcoverflow/app/controllers/questions_controller.rb
+++ dbcoverflow/app/controllers/questions_controller.rb
@@ -1,65 +1,69 @@
-class QuestionsController < ApplicationController	
-	before_action :find_question, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+class QuestionsController < ApplicationController
+  before_action :find_question, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
 
-	def index
-		if session[:user_id]
-			@session = User.find(session[:user_id])
-		end
-		@question = Question.new
-		@questions = Question.all
-	end
+  def index
+    if session[:user_id]
+      @session = User.find(session[:user_id])
+    end
+    @question = Question.new
+    @questions = Question.all
+  end
 
-	def show
-		@answers = @question.answers
-		@vote = Vote.new
-	    @comment = Comment.new
-	    @answer = Answer.new
-	end
+  def show
+      @answers = @question.answers
+      @vote = Vote.new
+      @comment = Comment.new
+      @answer = Answer.new
+  end
 
-	def create
-		@user = User.find(session[:user_id])
-		@question = Question.create(body: params[:question][:body])
-		@user.questions << @question
-		@question.vote = Vote.create
-		redirect_to root_path
-	end
+  def create
+    @user = User.find(session[:user_id])
+    @question = Question.create(body: params[:question][:body])
+    @user.questions << @question
+    @question.vote = Vote.create
+    redirect_to root_path
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-		@question.body = params[:question][:body]
-		@question.save
+  def update
+    @question.body = params[:question][:body]
+    @question.save
 
-		redirect_to root_path
-	end
+    redirect_to root_path
+  end
 
-	def destroy
-		@question.destroy
+  def destroy
+    @question.destroy
 
-		redirect_to root_path
-	end
+    redirect_to root_path
+  end
 
-	def upvote
-		@question.vote.score += 1
-		@question.vote.save
-		redirect_to question_path(@question)
-	end
+  def upvote
+    @question.vote.score += 1
+    @question.vote.save
+    redirect_to question_path(@question)
+  end
 
-	def downvote
-		@question.vote.score -= 1
-		@question.vote.save
-		redirect_to question_path(@question)
-	end
+  def downvote
+    @question.vote.score -= 1
+    @question.vote.save
+    redirect_to question_path(@question)
+  end
 
-	private
+  private
 
-	def question_params
-		params.require(:question).permit(:body)
-	end
+  def question_params
+    params.require(:question).permit(:body)
+  end
 
-	def find_question
-		@question = Question.find(params[:id])
-	end
+  def find_question
+    if Question.exists?(params[:id])
+      @question = Question.find(params[:id])
+    else
+      redirect_to root_path
+    end
+  end
 
 end
